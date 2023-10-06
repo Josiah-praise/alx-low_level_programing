e3ac39bff941e66c50caf632c21dc55a8ad4061e
josiah-praise <praisechinonso21@gmail.com>
2023-10-06 23:57:17 +0100

    fixed bug

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *Description: Write a program that prints the alphabet
+ *in lowercase, followed by a new line
+ *Return: 0 (sucessful)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	for (; alpha <= 'z'; alpha++)
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
+
