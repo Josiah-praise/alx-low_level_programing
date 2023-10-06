a06d2d4cdbc26f6d787b36f46cb3add17cf723a0
josiah-praise <praisechinonso21@gmail.com>
2023-10-07 00:54:49 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: Write a program that prints all the numbers of base 16,
+ *followed by a new line.
+ *Return: 0 (sucessful)
+ */
+int main(void)
+{
+	char alpha, number;
+
+	for (alpha = '0'; alpha <= 'f'; alpha++)
+	{
+		if ((alpha <= '9') || ((alpha >= 'a') && (alpha <= 'f')))
+			putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
