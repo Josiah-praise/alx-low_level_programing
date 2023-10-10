52e944900df38095de2c07374ee8d23e5155ad58
josiah-praise <praisechinonso21@gmail.com>
2023-10-10 13:38:42 +0100

    fixed bug

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,8 +4,12 @@
  *Description: display _putchar
  *Return: 0 (sucessful)
  */
-int _putchar(void)
+int _putchar()
 {
-	write(1, "_putchar\n", 9);
+	char string[10] = "_putchar\n";
+	int len;
+
+	len = sizeof(string)/sizeof(string[0]);
+	write(1, string, len);
 	return (0);
 }
