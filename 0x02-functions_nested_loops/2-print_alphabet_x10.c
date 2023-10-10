0fb24d4e61355d8780518dba5f581234677b4244
josiah-praise <praisechinonso21@gmail.com>
2023-10-10 15:23:42 +0100

    void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *2-print_alphabet_x10 - print alphabet
+ *Description: uses the printalphabet function 10x
+ *Return: Void (no return value)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+
+	for (; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(int);
-
+void print_alphabet_x10(void);
 void print_alphabet(void);
 
 #endif
