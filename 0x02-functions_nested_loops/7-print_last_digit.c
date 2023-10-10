8f32ab1e71fca67e1300ae104bcb4270ae84b42a
josiah-praise <praisechinonso21@gmail.com>
2023-10-10 17:21:19 +0100

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_last_digit - print last digit
+ *@c: integer variable
+ *Description: a function that prints the last digit of a number
+ *Return: value of the last digit
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _islower(int c);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 int _putchar(int);
 int _abs(int);
 int print_sign(int n);
