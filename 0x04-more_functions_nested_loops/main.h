2603c8a90424c6af5e74bbcd4bd2c1305345e9cc
josiah-praise <praisechinonso21@gmail.com>
2023-10-12 18:52:59 +0100

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _isupper(int c);
-
+void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);
 void print_line(int n);
