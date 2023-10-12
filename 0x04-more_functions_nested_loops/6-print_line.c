179436bf69d20e0d33ca22c23333076606f3dd27
josiah-praise <praisechinonso21@gmail.com>
2023-10-12 16:00:15 +0100

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_line - Name of funciton
+ *Return: no return value
+ *@n: integer parameter n
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i < (n + 1); i++)
+		_putchar('_');
+}
