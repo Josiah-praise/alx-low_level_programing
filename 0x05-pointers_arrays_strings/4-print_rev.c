8cf3dc2b66f9087bd29d78055b3c8f50e7461711
josiah-praise <praisechinonso21@gmail.com>
2023-10-16 19:33:20 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+  *print_rev - output function
+  *@s: character pointer parameter
+  *Return: no return value
+  */
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+	for (; i >= 0; i--)
+		_putchar(i);
+	_putchar('\n');
+}
+
+/**
+ *_strlen - helper function
+ *@s: character pointer;
+ *Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+		continue;
+	return (size);
+}
