917e27205d66107651550e413e73f4d763738010
josiah-praise <praisechinonso21@gmail.com>
2023-10-16 23:49:56 +0100

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+/**
+ *_strcpy - output function
+ *@dest: character pointer parameter
+ *@src: character pointer parameter
+ *Return: pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
