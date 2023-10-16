74fa50079eba800945e557d6db16b532387a9536
josiah-praise <praisechinonso21@gmail.com>
2023-10-16 22:04:48 +0100

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+/**
+  *_strlen - helper function
+  *@s: character pointer;
+  *Return: lenght of the string
+  */
+int _strlen(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+		continue;
+	return (size);
+}
+
+/**
+ *puts2 - helper function
+ *@str: character pointer;
+ *Return: no return value
+ */
+void puts2(char *str)
+{
+	int i, last_index;
+
+	last_index = _strlen(str) - 1;
+
+	for (i = 0; i <= last_index; i += 2)
+		_putchar(str[0]);
+	_putchar('\n');
+}
