1e8fe11e0b350cf0cf2f992c0a9f43d671953b72
josiah-praise <praisechinonso21@gmail.com>
2023-10-17 00:19:39 +0100

    fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,18 @@
+#include "main.h"
+/**
+  *_strlen - helper function
+  *@s: character pointer;
+  *Return: lenght of the string
+  */
+int _strlen(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+		continue;
+	return (size);
+}
+
 /**
  *_strcpy - output function
  *@dest: character pointer parameter
@@ -6,6 +21,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	*dest = *src;
+	int len, i;
+
+	len = _strlen(src);
+
+	for (i = 0; i < len; i++)
+		*(dest + i) = src[i];
+	*(dest + len) = '\0';
 	return (dest);
 }
