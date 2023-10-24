0e786b915dc7b1b9f9a7613aef5a1d508322d298
josiah-praise <praisechinonso21@gmail.com>
2023-10-24 05:01:24 +0100

    fixed bugs

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,8 +5,13 @@
 */
 void print_chessboard(char (*a)[8])
 {
-    /*Find the number of rows*/
-    int r;
+	int i, j;
 
-    
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+	}
 }
