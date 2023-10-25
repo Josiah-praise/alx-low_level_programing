89af4f844d0af9185ff58ccdac459f98dcc60104
josiah-praise <praisechinonso21@gmail.com>
2023-10-25 14:53:51 +0100

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+/**
+  *_strlen - helper function
+  *@s: character pointer;
+  *Return: lenght of the string
+  */
+int _strlen(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+		continue;
+	return (size);
+}
+
+
+/**
+ *palindrome - helper function
+ *Return: 1 if s a palindrome, 0 otherwise
+ *@s: pointer to character array
+ *@len: length of string - 1(the last index of the array)
+ *@start: the first index
+*/
+int palindrome(char *s, int len, int start)
+{
+	if (start >= len)
+		return (1);
+	if (s[len] == s[start])
+		return (palindrome(s, (len - 1), (start + 1)));
+	else
+		return (0);
+}
+
+
+/**
+ *is_palindrome - output function
+ *Return: 1 if s a palindrome, 0 otherwise
+ *@s: pointer to character array
+*/
+int is_palindrome(char *s)
+{
+	return (palindrome(s, (_strlen(s) - 1), 0));
+}
