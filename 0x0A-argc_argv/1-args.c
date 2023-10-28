9ce1896ae5f8f23a4a3ae45af655d269c3b19b5d
josiah-praise <praisechinonso21@gmail.com>
2023-10-28 02:25:50 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (sucessful)
+ * @argc: integer
+ * @argv: pointer to array
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
