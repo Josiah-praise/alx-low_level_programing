78979f22944a7b5bb9a0c5a9dc0d2657d4a75e01
josiah-praise <praisechinonso21@gmail.com>
2023-10-28 02:02:02 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (sucessful)
+ * @argc: integer
+ * @argv: commmand line arguments
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
