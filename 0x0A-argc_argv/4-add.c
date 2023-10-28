e4fc32321749aa2e5fa4f928766cb30c60884c4b
josiah-praise <praisechinonso21@gmail.com>
2023-10-28 04:13:39 +0100

    fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,36 +2,49 @@
 #include <stdlib.h>
 
 /**
- *main - Entry point
- *Return: 0 (sucessfull)
- *@argc: integer
- *@argv: pointer to string
+ * _isdigit - Check for a digit (0 through 9).
+ * @a: Charater to check
+ * Return: 1 if a is a digit, return 0 otherwise.
+ */
+
+int _isdigit(int a)
+{
+	if (a >= '0' && a <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * main - Add the given numbers.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0 is succes.
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0, error = 0;
+	int i, j, sum = 0;
+	char *c = NULL;
 
 	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else
+
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		c = argv[i];
+		for (j = 0; c[j] != '\0'; j++)
 		{
-			if (!(argv[i][0] >= '0' && argv[i][0] <= '9'))
+			if (_isdigit(c[j]) != 1)
 			{
 				printf("Error\n");
-				error++;
-			}
-			else
-			{
-				sum += atoi(*(argv + i));
+				return (1);
 			}
 		}
-		if (!error)
-			printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
-
 }
