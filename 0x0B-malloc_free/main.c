c5a7714df5395dca7503ef68aa44488a11e427f1
josiah-praise <praisechinonso21@gmail.com>
2023-10-31 15:03:47 +0100

    done

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -4,16 +4,8 @@
 
 int main()
 {
- char *s;
-
-    s = str_concat(NULL, NULL);
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    
-    printf("%s\n", s);
-    free(s);
+ 
+    free(NULL);
+    printf("yo");
     return (0);
 }
