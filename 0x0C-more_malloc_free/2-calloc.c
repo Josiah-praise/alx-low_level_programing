bd67bab75aff77ae8d33fe79a9fca0a3b165fcef
Josiah-praise <praisechinonso21@gmail.com>
2023-11-03 02:19:43 +0100

    fixed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 
+/**
+ * _calloc - allocates space dynamically
+ * @nmemb: the number of elements as an unsigned account
+ * @size: the size in bytes as an unsined  int
+ * Return: pointer to array
+*/
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 	unsigned int i;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
@@ -12,9 +19,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
-	
+
 	return (ptr);
 }
