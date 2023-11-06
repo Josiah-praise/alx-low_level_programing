ce63d4fedc84a4f3dd4d83110ab2c2162c41b1e7
Josiah-praise <praisechinonso21@gmail.com>
2023-11-06 22:50:29 +0100

    problems o

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,10 +6,14 @@
  * @name: a pointer to a string
  * @age: age as a floating point number
  * @owner: pointer to a string
+ * Return: No return Value
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-}
+	if (d)
+		{
+			d->name = name;
+			d->age = age;
+			d->owner = owner;
+		}
+	}
