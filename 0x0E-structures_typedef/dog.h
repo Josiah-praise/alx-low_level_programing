d8b9cccdf8b2ee30324d3cb9d73e0de36953a513
Josiah-praise <praisechinonso21@gmail.com>
2023-11-06 22:16:20 +0100

    added doh.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+/**
+ * struct dog - a dog structure
+ * @name: the dog name as
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+#endif /*DOG_H*/
