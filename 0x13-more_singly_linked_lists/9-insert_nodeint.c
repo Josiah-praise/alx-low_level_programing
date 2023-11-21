448d7f72265a4c4e047a10fdb933c03557ec20a0
Josiah-praise <praisechinonso21@gmail.com>
2023-11-21 13:00:22 +0100

    fixed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	/*if you want to insert at index 0*/
-	if (idx == 0 && *head == NULL)
+	if (idx == 0 && *head != NULL)
 	{
 		new_node->next = current;
 		*head = new_node;
