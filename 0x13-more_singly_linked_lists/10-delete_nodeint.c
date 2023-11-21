f3ba5e17bb115397fc0179c4d9d729a1133d6c41
Josiah-praise <praisechinonso21@gmail.com>
2023-11-21 12:27:27 +0100

    fixed seg fault

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current = NULL, *prev =NULL;
+	listint_t *current = NULL, *prev = NULL;
 	unsigned int idx = 0;
 
 	if (head == NULL)
