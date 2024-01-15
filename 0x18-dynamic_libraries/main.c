25b4bcb1cf3fea3c9595ba3786a4abf363e0ec0a
josiah-praise <praisechinonso21@gmail.com>
2024-01-15 12:48:26 +0100

    fixed bugs

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+    char arr[] = "I could have killed of people";
+    printf("Len: %d\n", _strlen(arr));
+    return (0);
+}
---
+++ 0x18-dynamic_libraries/src/create
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -Werror -pedantic -std=gnu89 -Wextra -c *.c -fpic
+gcc *.o -shared -o liball.so
