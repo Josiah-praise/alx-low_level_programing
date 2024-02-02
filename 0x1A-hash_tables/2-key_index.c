dd657f5a8a6017cb43be2aae03ad8c29b793a3d9
josiah-praise <praisechinonso21@gmail.com>
2024-02-02 09:44:12 +0100

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  * key_index - gives you the index of a key
  * @key: the key
  * @size: array size
+ * Return: key index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
